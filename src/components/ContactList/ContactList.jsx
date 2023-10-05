276cdd12007d395efb20be66cbea7c2532650b58
ADudys <ania.dudys99@gmail.com>
2023-10-06 01:31:46 +0200

    Fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,12 @@
-import PropTypes from "prop-types"
-import css from './ContactList.module.css'
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
 
 export const ContactList = ({ contacts, children, deleteContact }) => {
-    return (
-      <div className={css.contacts}>
-        <h2>Contacts</h2>
-        {children}
+  return (
+    <div className={css.contacts}>
+      <h2>Contacts</h2>
+      {children}
+      {contacts.length > 0 ? (
         <ul className={css.contacts__list}>
           {contacts.map(({ id, name, number }) => (
             <li className={css.contacts__item} key={id}>
@@ -22,17 +23,20 @@ export const ContactList = ({ contacts, children, deleteContact }) => {
             </li>
           ))}
         </ul>
-      </div>
-    );
-  };
-  
-  ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ),
-    deleteContact: PropTypes.func,
-  };
+      ) : (
+        <p> No contacts added to the list</p>
+      )}
+    </div>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  deleteContact: PropTypes.func,
+};
