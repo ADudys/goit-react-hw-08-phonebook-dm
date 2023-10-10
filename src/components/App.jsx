50bce7b5b8cb4c2042c0dad57a2c1fa115fab97e
ADudys <ania.dudys99@gmail.com>
2023-10-10 10:53:23 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,19 @@ const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
 
 const App = () => {
   const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState(() => parsedContacts ?? []);
+  const [contacts, setContacts] =  useState(
+    () =>
+      parsedContacts ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        { id: 'id-5', name: 'Ihor Kalyta', number: '577-99-77' },
+      ]
+  );;
 
-  useEffect(() => setContacts(parsedContacts));
+  useEffect(() => {localStorage.setItem('contacts', JSON.stringify(contacts));
+}, [contacts]);
 
   function addContact(newContact) {
     const loweredName = newContact.name.toLowerCase().trim();
@@ -22,17 +32,19 @@ const App = () => {
     ) {
       return alert(`${newContact.name} is already in Your contacts!`);
     }
-    if (contacts.length === 0) {
-      localStorage.removeItem('contacts');
-    }
+
     if (
       this.state.contacts.find(contact => contact.number === newContact.number)
     ) {
       return alert(`${newContact.number} is already in Your contacts!`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [...contacts, newContact],
-      }));
+      if (contacts.lenght === 0) {
+        localStorage.removeItem('contacts');
+      } else {
+        this.setState(({ contacts }) => ({
+          contacts: [...contacts, newContact],
+        }));
+      }
     }
   }
   function deleteContact(id) {
