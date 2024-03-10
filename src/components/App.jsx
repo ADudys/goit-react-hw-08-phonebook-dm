75d5acaa33e86c7d97fab4a846d6136acab853dc
ADudys <ania.dudys99@gmail.com>
2024-03-10 13:58:40 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,11 @@ import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
 import ContactForm from './ContactForm/ConstactForm';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectError, selectIsLoading } from 'store/selectors';
 import { fetchContacts } from 'store/operations';
 
 const App = () => {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+
 
   useEffect(() => {
     dispatch(fetchContacts());
