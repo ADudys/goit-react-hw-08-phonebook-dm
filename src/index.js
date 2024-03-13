0b18742d67b4a84c53e0313860e5487367e66dfc
ADudys <121581322+ADudys@users.noreply.github.com>
2024-03-13 10:07:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <HelmetProvider>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <BrowserRouter basename="/goit-react-hw-08-phonebook-dm">
             <App />
           </BrowserRouter>
         </HelmetProvider>
